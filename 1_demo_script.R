5c4c21b2eb10d0b06689035e6c1cf4397f46961c
Andrea Sánchez Tapia <4183062+AndreaSanchezTapia@users.noreply.github.com.>
2024-12-05 11:19:56 -0500

    add script skeleton

---
+++ 1_demo_script.R
@@ -0,0 +1,13 @@
+# assuming the package is installed
+# library(remotes)
+# install_github("GlobalFishingWatch/gfwr")
+# Load
+library(gfwr)
+
+# Load other packages we will use
+library(dplyr)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggplot2)
+library(tidyr)
+
